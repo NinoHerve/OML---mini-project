063f19fca77888d32a72db1d816fa1f855796c06
Juan Garcia Giraldo <juan.garciagiraldo@student.manchester.ac.uk>
2024-04-05 00:17:01 +0100

    updating finetunning tasks

---
+++ src/preprocessing.py
@@ -0,0 +1,135 @@
+from datasets import load_dataset
+
+
+def get_sst2_dataset(tokenizer):
+    dataset = load_dataset("glue", "sst2")
+    encoded_dataset = dataset.map(lambda sample: tokenizer(sample["sentence"], truncation=True), batched=True)
+    return dataset, encoded_dataset
+
+def get_nli_dataset(tokenizer):
+    dataset = load_dataset("glue", "mnli")
+    dataset["validation"], dataset["test"] = dataset.pop("validation_matched"), dataset.pop("test_matched")
+    dataset.pop("validation_mismatched")
+    dataset.pop("test_mismatched")
+    encoded_dataset = dataset.map(lambda sample: tokenizer(sample["premise"], sample["hypothesis"], truncation=True), batched=True)
+    return dataset, encoded_dataset
+
+def get_ner_dataset(tokenizer):
+    dataset = load_dataset("conll2003")
+    encoded_dataset = dataset.map(lambda sample: tokenize_and_align_labels(tokenizer, sample), batched=True)
+    return dataset, encoded_dataset
+
+def get_squad_dataset(tokenizer):
+    dataset = load_dataset("squad")
+    encoded_dataset = dataset.map(lambda sample: preprocess_squad_dataset(tokenizer, sample), batched=True, remove_columns=dataset["train"].column_names)
+    return dataset, encoded_dataset
+
+
+# Modified from RobertaForTokenClassification example notebook from Hugging Face
+# Link: https://huggingface.co/docs/transformers/en/model_doc/roberta#roberta
+def tokenize_and_align_labels(tokenizer, examples):
+    tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
+
+    labels = []
+    for i, label in enumerate(examples["ner_tags"]):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)
+        previous_word_idx = None
+        label_ids = []
+        for word_idx in word_ids:
+            # Special tokens have a word id that is None. We set the label to -100 so they are automatically
+            # ignored in the loss function.
+            if word_idx is None:
+                label_ids.append(-100)
+            # We set the label for the first token of each word.
+            elif word_idx != previous_word_idx:
+                label_ids.append(label[word_idx])
+            # For the other tokens in a word, we set the label to either the current label or -100, depending on
+            # the label_all_tokens flag.
+            else:
+                label_ids.append(label[word_idx])
+            previous_word_idx = word_idx
+
+        labels.append(label_ids)
+
+    tokenized_inputs["labels"] = labels
+    return tokenized_inputs
+
+
+# Modified from RobertaForQuestionAnswering  example notebook from Hugging Face
+# Link: https://huggingface.co/docs/transformers/en/model_doc/roberta#roberta
+def preprocess_squad_dataset(tokenizer, examples, max_length = 384, doc_stride = 128):
+    # Some of the questions have lots of whitespace on the left, which is not useful and will make the
+    # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
+    # left whitespace
+    examples["question"] = [q.lstrip() for q in examples["question"]]
+
+    # Tokenize our examples with truncation and padding, but keep the overflows using a stride. This results
+    # in one example possible giving several features when a context is long, each of those features having a
+    # context that overlaps a bit the context of the previous feature.
+    tokenized_examples = tokenizer(
+        examples["question"],
+        examples["context"],
+        truncation="only_second",
+        max_length=max_length,
+        stride=doc_stride,
+        return_overflowing_tokens=True,
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    # Since one example might give us several features if it has a long context, we need a map from a feature to
+    # its corresponding example. This key gives us just that.
+    sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+    # The offset mappings will give us a map from token to character position in the original context. This will
+    # help us compute the start_positions and end_positions.
+    offset_mapping = tokenized_examples.pop("offset_mapping")
+
+    # Let's label those examples!
+    tokenized_examples["start_positions"] = []
+    tokenized_examples["end_positions"] = []
+
+    for i, offsets in enumerate(offset_mapping):
+        # We will label impossible answers with the index of the CLS token.
+        input_ids = tokenized_examples["input_ids"][i]
+        cls_index = input_ids.index(tokenizer.cls_token_id)
+
+        # Grab the sequence corresponding to that example (to know what is the context and what is the question).
+        sequence_ids = tokenized_examples.sequence_ids(i)
+
+        # One example can give several spans, this is the index of the example containing this span of text.
+        sample_index = sample_mapping[i]
+        answers = examples["answers"][sample_index]
+        # If no answers are given, set the cls_index as answer.
+        if len(answers["answer_start"]) == 0:
+            tokenized_examples["start_positions"].append(cls_index)
+            tokenized_examples["end_positions"].append(cls_index)
+        else:
+            # Start/end character index of the answer in the text.
+            start_char = answers["answer_start"][0]
+            end_char = start_char + len(answers["text"][0])
+
+            # Start token index of the current span in the text.
+            token_start_index = 0
+            while sequence_ids[token_start_index] != 1:
+                token_start_index += 1
+
+            # End token index of the current span in the text.
+            token_end_index = len(input_ids) - 1
+            while sequence_ids[token_end_index] != 1:
+                token_end_index -= 1
+
+            # Detect if the answer is out of the span (in which case this feature is labeled with the CLS index).
+            if not (offsets[token_start_index][0] <= start_char and offsets[token_end_index][1] >= end_char):
+                tokenized_examples["start_positions"].append(cls_index)
+                tokenized_examples["end_positions"].append(cls_index)
+            else:
+                # Otherwise move the token_start_index and token_end_index to the two ends of the answer.
+                # Note: we could go after the last offset if the answer is the last word (edge case).
+                while token_start_index < len(offsets) and offsets[token_start_index][0] <= start_char:
+                    token_start_index += 1
+                tokenized_examples["start_positions"].append(token_start_index - 1)
+                while offsets[token_end_index][1] >= end_char:
+                    token_end_index -= 1
+                tokenized_examples["end_positions"].append(token_end_index + 1)
+
+    return tokenized_examples
